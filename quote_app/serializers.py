09fedd0c56143642baac3f98f4414f224aa6334c
Rojin4455 <rojinsaji35@gmail.com>
2025-08-19 19:55:21 +0530

    bugs resolved

--- quote_app/serializers.py
+++ quote_app/serializers.py
@@ -234,9 +234,10 @@ class CustomerSubmissionDetailSerializer(serializers.ModelSerializer):
     def get_fields(self):
         fields = super().get_fields()
         request = self.context.get('request')
-        if request and request.method in ['PATCH', 'PUT']:
-            fields['customer_address'].read_only = True
+        # if request and request.method in ['PATCH', 'PUT']:
+        #     fields['customer_address'].read_only = True
         return fields
+
 
 class CustomerServiceSelectionDetailSerializer(serializers.ModelSerializer):
     """Detailed serializer for service selections"""
